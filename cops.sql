f1486dae80307cc5b6d8a3e4e31087ab9bc043fb
Protsenko Roman <romapr70@gmail.com>
2022-04-27 15:11:04 +0300

    Initially commit.

---
+++ cops.sql
@@ -0,0 +1,36 @@
+create table cops (
+        id BIGSERIAL PRIMARY KEY NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        birth_date DATE NOT NULL,
+        country VARCHAR(100) NOT NULL
+    );
+insert into cops (name, birth_date, country) values ('Chris Graham', '25.8.1963', 'Colombia');
+insert into cops (name, birth_date, country) values ('Walter Thompson', '20.3.1956', 'Hashemite Kingdom of Jordan');
+insert into cops (name, birth_date, country) values ('Richard Johnston', '24.10.1990', 'Latvia');
+insert into cops (name, birth_date, country) values ('Joe Henderson', '11.8.1955', 'Portugal');
+insert into cops (name, birth_date, country) values ('Daniel Cox', '2.7.1960', 'Oman');
+insert into cops (name, birth_date, country) values ('Brian Hill', '18.8.1956', 'Libya');
+insert into cops (name, birth_date, country) values ('Lloyd Myers', '20.11.1968', 'Thailand');
+insert into cops (name, birth_date, country) values ('Charles Smith', '18.11.1965', 'Norway');
+insert into cops (name, birth_date, country) values ('William Lopez', '3.10.1982', 'Paraguay');
+insert into cops (name, birth_date, country) values ('Eugene Robertson', '4.5.1958', 'Iceland');
+insert into cops (name, birth_date, country) values ('Phillip Barrett', '19.5.1970', 'Iraq');
+insert into cops (name, birth_date, country) values ('Alberto Bryant', '27.5.1972', 'Republic of Lithuania');
+insert into cops (name, birth_date, country) values ('Mark Lewis', '26.5.1983', 'Jamaica');
+insert into cops (name, birth_date, country) values ('Wade Powers', '21.7.1985', 'Andorra');
+insert into cops (name, birth_date, country) values ('Scott Jordan', '15.1.1994', 'Bahamas');
+insert into cops (name, birth_date, country) values ('Frank McGuire', '24.6.1976', 'Ukraine');
+insert into cops (name, birth_date, country) values ('Jeffrey Williams', '27.1.1996', 'Algeria');
+insert into cops (name, birth_date, country) values ('Harold King', '7.10.1962', 'Nepal');
+insert into cops (name, birth_date, country) values ('Bill Spencer', '7.2.1968', 'Azerbaijan');
+insert into cops (name, birth_date, country) values ('Adam Jones', '22.5.1994', 'Estonia');
+insert into cops (name, birth_date, country) values ('William Massey', '6.8.1995', 'Bosnia and Herzegovina');
+insert into cops (name, birth_date, country) values ('Jorge Daniels', '18.2.1956', 'Slovakia');
+insert into cops (name, birth_date, country) values ('Robert Foster', '25.5.1997', 'Guinea');
+insert into cops (name, birth_date, country) values ('Stephen Dixon', '13.9.1973', 'Kenya');
+insert into cops (name, birth_date, country) values ('Joshua Graham', '2.9.1989', 'Guadeloupe');
+insert into cops (name, birth_date, country) values ('Howard Jones', '27.3.1957', 'New Caledonia');
+insert into cops (name, birth_date, country) values ('David West', '7.2.2000', 'Bangladesh');
+insert into cops (name, birth_date, country) values ('Jerry Hernandez', '12.10.1995', 'Sweden');
+insert into cops (name, birth_date, country) values ('Michael Wilson', '13.1.1956', 'French Polynesia');
+insert into cops (name, birth_date, country) values ('Walter Smith', '9.5.1960', 'Taiwan');
